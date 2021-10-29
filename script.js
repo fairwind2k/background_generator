aad41652129cdde6c72df203dfc5b16981765e53
fairwind2k <32002264+fairwind2k@users.noreply.github.com>
2021-10-29 15:27:55 +0200

    Update script.js
    Add a new function 'random'

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var button = document.getElementById("random");
+
+css.textContent = getComputedStyle(document.body).backgroundImage + ";";
 
 function setGradient() {
 	body.style.backgroundImage =
@@ -16,3 +19,25 @@ function setGradient() {
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
+
+function randomRgbColors() {
+	let arr = [];
+	for (let i = 0; i < 3; i++) {
+		arr[i] = Math.floor(Math.random() * 255);
+	};
+	var str = "rgb(" + arr[0] + ", " + arr[1] + ", " + arr[2] + ")";
+	return str;
+}
+
+button.addEventListener("click", function() {
+	let first = randomRgbColors();
+	let second = randomRgbColors();
+	body.style.backgroundImage =
+	 "linear-gradient(to right, "
+	 + first
+	 + ", "
+	 + second
+	 + ")" ;
+
+	 css.textContent = body.style.backgroundImage + ";"
+});
